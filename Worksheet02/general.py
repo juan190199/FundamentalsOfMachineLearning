75d18d9cefd7553bb6c86edb98cdb4912309e94b
juan190199 <juanmanueldasilvatome@gmail.com>
2020-11-25 00:18:25 +0100

    Vectorized implementation of euclidean distance

--- Worksheet02/general.py
+++ Worksheet02/general.py
@@ -1,7 +1,17 @@
 import numpy as np
+import numpy.testing as nt
 
 
-def euclidean_distance(x, y):
-    distance = np.sqrt(np.sum(np.square(np.subtract(x, y))))
-    assert distance == np.linalg.norm(np.subtract(x, y))
-    return distance
+def euclidean_distance(x, y, loop=None):
+    if loop == 2:
+        distance = np.sqrt(np.sum(np.square(np.subtract(x, y))))
+        nt.assert_array_equal(distance, np.linalg.norm(np.subtract(x, y)))
+        return distance
+    if loop == 1:
+        distance = np.sqrt(np.sum(np.square(np.subtract(x, y)), axis=1))
+        nt.assert_array_equal(distance, np.linalg.norm(np.subtract(x, y), axis=1))
+        return distance
+    else:
+        distance = np.sqrt((np.square(x[:, np.newaxis] - y).sum(axis=2)))
+        # distance = -2 * np.dot(x, y.T) + np.sum(np.square(y), axis=1) + np.sum(np.square(x), axis=1)[:, np.newaxis]
+        return distance
--- Worksheet02/main.py
+++ Worksheet02/main.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_digits
 from sklearn import model_selection
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy.testing as nt
 
 import general as gen
 
@@ -19,8 +20,24 @@ def dist_loop(training, test):
     distances = np.empty(shape=(n_instances, batch_size))
     for i in range(n_instances):
         for j in range(batch_size):
-            dist = gen.euclidean_distance(training[i, :], test[j, :])
-            distances[i][j]
+            dist = gen.euclidean_distance(training[i, :], test[j, :], loop=2)
+            distances[i][j] = dist
+    return distances
+
+
+def dist_vec_one_loop(training, test):
+    """
+
+    :param training: matrix of shape (n_instances, D). n_instances = number of instances in the training set.
+    D = pixels per image
+    :param test: matrix of shape (batch_size, D). batch_size = number of instances in the test set. D = pixels per image
+    :return: matrix of shape (n_instances, batch_size)
+    """
+    n_instances = training.shape[0]
+    batch_size = test.shape[0]
+    distances = np.empty(shape=(n_instances, batch_size))
+    for i in range(batch_size):
+        distances[:, i] = gen.euclidean_distance(training, test[i, :], loop=1)
     return distances
 
 
@@ -32,11 +49,8 @@ def dist_vec(training, test):
     :param test: matrix of shape (batch_size, D). batch_size = number of instances in the test set. D = pixels per image
     :return: matrix of shape (n_instances, M)
     """
-    n_instances = training.shape[0]
-    batch_size = test.shape[0]
-    distances = np.empty(shape=(n_instances, batch_size))
-    for i in range(n_instances):
-        dist = np.apply_along_axis(gen.euclidean_distance, axis=0, args=(training[i, :], test))
+    distances = gen.euclidean_distance(training, test)
+    return distances
 
 
 def main():
@@ -49,6 +63,7 @@ def main():
     target = digits['target']
     target_names = digits['target_names']
 
+    print(digits['DESCR'])
     print(data.dtype)
     print(data.shape)
     """
@@ -69,16 +84,17 @@ def main():
         assert 2 == len(image.shape)
         ax.imshow(image, cmap=plt.cm.gray_r, interpolation='bicubic')
         ax.set_title('Training: %i' % label)
-    plt.show()
+    # plt.show()
 
     X_train, X_test, Y_train, Y_test = model_selection.train_test_split(digits.data,
                                                                         digits.target,
                                                                         test_size=0.4,
                                                                         random_state=0)
 
-    distance_loop = dist_loop(X_train, X_test)
+    # distance_loop = dist_loop(X_train, X_test)
+    distance_vec_one_loop = dist_vec_one_loop(X_train, X_test)
     distance_vec = dist_vec(X_train, X_test)
-    # assert distance_loop == distance_vec
+    nt.assert_array_equal(distance_vec_one_loop, distance_vec)
 
 
 if __name__ == '__main__':
