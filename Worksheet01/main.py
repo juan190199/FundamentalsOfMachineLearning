9eda54f6cb8ca3e0313cf49d4efaf2ac39f76fac
juan190199 <juanmanueldasilvatome@gmail.com>
2020-11-24 12:51:26 +0100

    Plots task 2 done

--- Worksheet01/main.py
+++ Worksheet01/main.py
@@ -2,10 +2,10 @@ import general as gen
 
 import numpy as np
 import pandas as pd
-from IPython.display import display
+import matplotlib.pyplot as plt
 
 
-def threshold_classifier(X, type, threshold=None, error=False):
+def threshold_classifier(type, X=None, threshold=None, error=False):
     """
     :param X: Numpy array with test set to be classified
     :param type: Type of classifier to be applied
@@ -37,24 +37,55 @@ def threshold_classifier(X, type, threshold=None, error=False):
             return np.ones(len(X))
 
 
-def calculate_ose(type, batch, n_data_sets, threshold=None):
+def calculate_error(type, error=False, batch=None, n_data_sets=None, threshold=None):
     """
     Calculate out-of-sample errors given number of data sets
     :param type: Classifier type
+    :param error: Boolean variable. True to calculate analytical error of the classifier
     :param batch: size of the test set
     :param n_data_sets:
     :param threshold: threshold of the classifier
     :return:
     """
-    oses = np.zeros(n_data_sets)
-    for i in range(n_data_sets):
-        test_set = gen.create_data(batch)
-        prediction = threshold_classifier(test_set[:, 0], type=type, threshold=threshold)
-        n_errors = np.sum(np.abs(np.subtract(prediction, test_set[:, 1])))
-        ose = n_errors / batch
-        oses[i] = ose
+    if error is False:
+        oses = np.zeros(n_data_sets)
+        for i in range(n_data_sets):
+            test_set = gen.create_data(batch)
+            prediction = threshold_classifier(type=type, X=test_set[:, 0], threshold=threshold)
+            n_errors = np.sum(np.abs(np.subtract(prediction, test_set[:, 1])))
+            ose = n_errors / batch
+            oses[i] = ose
 
-    return oses
+        return oses
+    else:
+        a_error = threshold_classifier(type=type, error=error, threshold=threshold)
+        return a_error
+
+
+def plot_error_rate(dfs, thresholds):
+    cont_thresholds = np.linspace(0, 1, 100)
+    error_A = calculate_error(type='A', error=True, threshold=cont_thresholds)
+    error_B = calculate_error(type='B', error=True, threshold=cont_thresholds)
+    errors = [error_A, error_B]
+
+    for name, df, true_error in zip(["A", "B"], dfs, errors):
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+        plt.suptitle(f"Classifier {name}")
+
+        plt.sca(ax1)
+        plt.ylim(0, 1)
+        plt.title("Error rate mean")
+        df["Mean"].plot(marker="o", yerr=df["Std"], lw=1, ax=ax1)
+        plt.plot(cont_thresholds, true_error, "k--")
+        plt.xticks([0] + thresholds + [1])
+
+        plt.sca(ax2)
+        plt.title("Error rate std")
+        df["Std"].T.plot(marker="o", ax=ax2, logx=True, logy=True)
+
+        plt.tight_layout()
+
+        plt.show()
 
 
 def task1():
@@ -68,26 +99,40 @@ def task2():
 
     classifier_A = []
     classifier_B = []
+    # error_A = []
+    # error_B = []
 
     for threshold in thresholds:
         for batch in batch_sizes:
-            error_rates_A = calculate_ose(type='A', batch=batch, n_data_sets=10, threshold=threshold)
+            error_rates_A = calculate_error(type='A', batch=batch, n_data_sets=10, threshold=threshold)
             classifier_A.append({"$x_0$": threshold,
                                  "Batch size": batch,
                                  "Mean": error_rates_A.mean(),
                                  "Std": error_rates_A.std()
                                  })
 
-            error_rates_B = calculate_ose(type='B', batch=batch, n_data_sets=10, threshold=threshold)
+            error_rates_B = calculate_error(type='B', batch=batch, n_data_sets=10, threshold=threshold)
             classifier_B.append({"$x_0$": threshold,
                                  "Batch size": batch,
                                  "Mean": error_rates_B.mean(),
                                  "Std": error_rates_B.std()
                                  })
+        # error_A.append({"$x_0$": threshold,
+        #                 "error": calculate_error(type='A', error=True, threshold=threshold)
+        #                 })
+        # error_B.append({"$x_0$": threshold,
+        #                 "error": calculate_error(type='B', error=True, threshold=threshold)
+        #                 })
 
     df_A = pd.DataFrame(classifier_A)
     df_A = df_A.groupby(["$x_0$", "Batch size"]).first().unstack()
-    df_A
+    # print(df_A)
+
+    df_B = pd.DataFrame(classifier_B)
+    df_B = df_B.groupby(["$x_0$", "Batch size"]).first().unstack()
+    # print(df_B)
+
+    plot_error_rate([df_A, df_B], thresholds)
 
 
 def task3():
