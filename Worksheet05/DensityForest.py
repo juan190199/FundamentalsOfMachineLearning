9d1ceebea6c6e6bfe5925870c845909804788fd5
juan190199 <juanmanueldasilvatome@gmail.com>
2021-01-02 16:04:42 +0100

    Implementation Density Forest

--- Worksheet05/DensityForest.py
+++ Worksheet05/DensityForest.py
@@ -1,16 +1,18 @@
-import DensityTree
+import Worksheet05.DensityTree as DensityTree
+
+import numpy as np
 
 
-class DensityForest():
+class DensityForest:
     def __init__(self, n_trees):
-        # create ensemble
+        # Create ensemble
         self.trees = [DensityTree.DensityTree() for i in range(n_trees)]
 
     def train(self, data, prior, n_min=20):
         for tree in self.trees:
-            # train each tree, using a bootstrap sample of the data
-            ... # your code here
+            bootstrap_data = data[np.random.choice(len(data), size=len(data))]
+            tree.train(bootstrap_data, prior, n_min)
 
     def predict(self, x):
-        # compute the ensemble prediction
-        return ... # your code here
+        # Compute the ensemble prediction
+        return np.mean([tree.predict(x) for tree in self.trees])
