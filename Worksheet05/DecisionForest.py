fde9598ee6cbec366ca2a850a4ba045a3292ffe8
juan190199 <juanmanueldasilvatome@gmail.com>
2021-01-02 16:04:50 +0100

    Implementation Decision Forest

--- Worksheet05/DecisionForest.py
+++ Worksheet05/DecisionForest.py
@@ -1,16 +1,19 @@
-import DecisionTree
+import Worksheet05.DecisionTree as DecisionTree
+
+import numpy as np
 
 
-class DecisionForest():
+class DecisionForest:
     def __init__(self, n_trees):
-        # create ensemble
+        # Create ensemble
         self.trees = [DecisionTree.DecisionTree() for i in range(n_trees)]
 
     def train(self, data, labels, n_min=0):
         for tree in self.trees:
-            # train each tree, using a bootstrap sample of the data
-            ... # your code here
+            # Train each tree, using a bootstrap sample of the data
+            bootstrap = np.random.choice(len(data), size=len(data))
+            tree.train(data[bootstrap], labels[bootstrap], n_min)
 
     def predict(self, x):
-        # compute the ensemble prediction
-        return ... # your code here
+        # Compute the ensemble prediction
+        return np.mean([tree.predict(x) for tree in self.trees], axis=0)
