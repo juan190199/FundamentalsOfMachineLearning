29facb90a8a6f8fe12059900df350d3c95de713c
juan190199 <juanmanueldasilvatome@gmail.com>
2021-01-11 17:38:57 +0100

    Referee is relevant to the main goal data preparation

--- Worksheet06/DataPreparation.py
+++ Worksheet06/DataPreparation.py
@@ -13,10 +13,26 @@ def data_preparation():
     # Only players that have an image
     df_has_image = df[pd.notnull(df['photoID'])]
 
-    # Groupping data by soccer player
-    df_aggregated = df_has_image.drop(['refNum', 'refCountry'], 1)
-    df_aggregated = df_aggregated.groupby(['playerShort', 'position'])[
-        'games', 'yellowCards', 'yellowReds', 'redCards'].sum()
+    """
+    What do the feature names (e.g. column games) stands for?
+    games: number of games in the player-referee dyad
+    A referee-player dyad describes the interactions between a particular ref and one player. 
+    This means that each row in the dataset is of a unique player-ref combination, 
+    listing all of the games by a given player with a particular referee at any point in his career. 
+    
+    Which irrelevant features might be dropped?
+    player, club, leagueCountry, birthday, height, weight, position, games, victories, ties, defeats, goals, photoID, 
+    refNum, refCountry, Alpha_3, meanIAT, nIAT, seIAT, meanExp, nExp, seExp
+    
+    What relevant features might be missing, but can be computed?
+    skinColor: Average of rater1 and rater2
+    redPerMatch: 
+    """
+
+    # Groupping data by soccer player and referee country
+    # df_aggregated = df_has_image.drop(['refNum', 'refCountry'], 1)
+    df_aggregated = df_has_image.groupby(['playerShort', 'position', 'refCountry'])[
+        'games', 'yellowCards', 'yellowReds', 'redCards', 'meanIAT', 'meanExp'].sum()
     df_aggregated = df_aggregated.reset_index()
 
     # Information of skin color for each player
@@ -30,7 +46,7 @@ def data_preparation():
                              how='left',
                              left_on='playerShort',
                              right_on='playerShort')
-    df_aggregated = df_aggregated.drop_duplicates('playerShort')
+    df_aggregated = df_aggregated.drop_duplicates(subset=['playerShort', 'refCountry'])
     df_aggregated = df_aggregated.reset_index(drop=True)
 
     return df_aggregated
